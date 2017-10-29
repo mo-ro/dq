8b148ddc8e29079e5ccb385ccfb5eb8f74334010
mo-ro <gainmoro@gmail.com>
2017-10-29 17:37:07 +0900

    攻撃システム完成

--- src/index.js
+++ src/index.js
@@ -7,26 +7,96 @@ class Status extends Component {
   constructor() {
     super();
     this.state = {
-      ally: "500",
-      enemy: "500"
+      ally_hp: "500",
+      enemy_hp: "1300",
+      phase: "select",
+      tagname: "",
+      enemy_damage: "",
+      ally_damage: ""
     }
-    this.attack = this.attack.bind(this);    
+    
+    this.attack = this.attack.bind(this);  
+    this.changeEnemyState = this.changeEnemyState.bind(this);
+    this.changeSelectState = this.changeSelectState.bind(this);
+    this.enemyAttack = this.enemyAttack.bind(this);
+    this.allyAttack = this.allyAttack.bind(this);
+  }
+    
+  getDamage() {
+    
+  }
+  
+  changeEnemyState() {
+    var damage = this.enemyAttack();
+    this.setState({enemy_damage: damage});
+    this.setState(function(state, cur) {
+      return {ally_hp: state.ally_hp - damage};
+    });
+    this.setState({phase: "enemy_attack"});
+  }
+  
+  changeSelectState() {
+    this.setState({phase: "select"})
+  }
+  
+  enemyAttack() {
+    return Math.floor( Math.random() * (110 - 80) ) + 80;
+  }
+  allyAttack() {
+    return Math.floor( Math.random() * (320 - 290) ) + 290;
   }
     
   attack() {
-    this.setState(function(state, cur) {
-      return {ally: state.ally - 10};
-    });
+    var setState = this.setState;
+    var self = this;
+    
+    
+    this.setState({phase: "attack"});
+    window.setTimeout(function() {
+      self.setState({tagname: document.forms.attack_form.attack_input.value});
+      var damage = self.allyAttack();
+      self.setState({ally_damage: damage});
+      self.setState({enemy_hp: self.state.enemy_hp - damage})
+      self.setState({phase: "ally_attack"});
+    }, 500);
+    
   }
   
   render() {
-    return(
-      <div className="display_wrapper">
-        <TeamMembers hp={this.state.ally}/>
-        <Enemy />
-        <Menu attack={this.attack}/>
-      </div>
-    )
+    if(this.state.phase === "select"){
+      return(
+        <div className="display_wrapper">
+          <TeamMembers hp={this.state.ally_hp}/>
+          <Enemy />
+          <Menu attack={this.attack}/>
+        </div>
+      )
+    }else if(this.state.phase === "attack"){
+      return(
+        <div className="display_wrapper">
+          <TeamMembers hp={this.state.ally_hp}/>
+          <Enemy />
+          <Form />
+        </div>
+      )
+    }else if(this.state.phase === "ally_attack") {
+      console.log(this.state.enemy_hp)
+      return(
+        <div className="display_wrapper">
+          <TeamMembers hp={this.state.ally_hp}/>
+          <Enemy />
+          <AllyAttackLog tagname={this.state.tagname} changeEnemyState={this.changeEnemyState} damage={this.state.ally_damage}/>
+        </div>
+      )
+    }else if(this.state.phase === "enemy_attack") {
+      return(
+        <div className="display_wrapper">
+          <TeamMembers hp={this.state.ally_hp}/>
+          <Enemy />
+          <EnemyAttackLog changeSelectState={this.changeSelectState} damage={this.state.enemy_damage}/>
+        </div>
+      )
+    }
   }
 }
 
@@ -38,13 +108,34 @@ function toFullWidth(value) {
 
 class TeamMembers extends Component {
   render() {
-    var hp = this.props.hp
+    var hp = this.props.hp;
+    var status_color = document.getElementById("App");
+    var borders = document.getElementsByClassName("border_style");
+    var sheets = document.styleSheets,
+    sheet = sheets[sheets.length - 1];
+    
+    if(hp <= 250 && hp > 100){
+      var color = "#f1933a";
+      status_color.style.color = color;
+      document.getElementById("App").style.borderColor = color;
+
+      sheet.insertRule('.menu_list:hover::after { border-left: 10px solid ' + color + ' }', sheet.cssRules.length);
+    }else if(hp <= 100) {
+      var color = "#e7261c";
+      
+      status_color.style.color = color;
+      document.getElementById("App").style.borderColor = color;
+      sheet.insertRule('.menu_list:hover::after { border-left: 10px solid ' + color + ' }', sheet.cssRules.length);
+      if(hp <= 0) {
+        hp = 0;
+      }
+    }
     return(
-      <div className="members">
+      <div className="members border_style">
         <ul>
-          <li>Ｈ<span>{toFullWidth(this.props.hp + "")}</span></li>
-          <li>Ｍ</li>
-          <li></li>
+          <li className="status_list">Ｈ<span>{toFullWidth(hp + "")}</span></li>
+          <li className="status_list">Ｍ<span>６</span></li>
+          <li className="status_list">も：<span>１</span></li>
         </ul>
       </div>
     )
@@ -65,14 +156,50 @@ class Menu extends Component {
   render() {
     return(
       <div className="menu">
-        <div className="menu_inner">
+        <div className="action_list border_style">
           <ul className="menu_lists">
             <li className="menu_list" onClick={this.props.attack}>たたかう</li>
-            <li className="menu_list">まほう</li>
-            <li className="menu_list">アイテム</li>
+            <li className="menu_list">じゅもん</li>
+            <li className="menu_list">どうぐ</li>
             <li className="menu_list">にげる</li>
           </ul>
-        </div> 
+        </div>
+        <div className="enemy_list border_style">
+        
+        </div>
+      </div>
+    )
+  }
+}
+
+class Form extends Component {
+  render() {
+    return(
+      <form id="attack_form">
+        <input type="text" id="attack_input" autoFocus autocomplete="off" />
+      </form>
+    )
+  }
+}
+
+class AllyAttackLog extends Component {
+  render() {
+    return(
+      <div className="ally_attack_log border_style" onClick={this.props.changeEnemyState}>
+        もろーの{this.props.tagname}こうげき！<br />
+        {toFullWidth(this.props.damage + "")}のダメージ！！
+      </div>
+    )
+  }
+}
+
+class EnemyAttackLog extends Component {
+  render() {
+    console.log(this.props.damage)
+    return(
+      <div className="enemy_attack_log border_style" onClick={this.props.changeSelectState}>
+        エネミーのこうげき！<br />
+        {toFullWidth(this.props.damage + "")}のダメージ！！
       </div>
     )
   }
